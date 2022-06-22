a94a41359ce0999ca9f53b006d63e5b4c7986bcf
Robert Egeland <robegeland@yahoo.com>
2022-06-22 16:37:41 -0500

    create user, concert, and artist table

---
+++ db/migrate/20220622213325_create_users_table.rb
@@ -0,0 +1,6 @@
+class CreateUsersTable < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      
+  end
+end
---
+++ db/migrate/20220622213336_create_concerts_table.rb
@@ -0,0 +1,4 @@
+class CreateConcertsTable < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
