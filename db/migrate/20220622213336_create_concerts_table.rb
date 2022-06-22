2312697f8ec92497950ad6827c08f74803f2a8d8
Robert Egeland <robegeland@yahoo.com>
2022-06-22 17:04:10 -0500

    fill in concert table/ migrate the tables

--- db/migrate/20220622213336_create_concerts_table.rb
+++ db/migrate/20220622213336_create_concerts_table.rb
@@ -1,4 +1,12 @@
 class CreateConcertsTable < ActiveRecord::Migration[6.1]
   def change
+    create_table :concerts do |t|
+      t.string :name
+      t.string :location
+      t.string :venue
+      t.integer :artists_id
+      t.integer :users_id
+      t.timestamps
+    end
   end
 end
