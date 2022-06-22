ba1f8b36b4359d25719cb028e167cba5086fd192
Robert Egeland <robegeland@yahoo.com>
2022-06-22 16:59:23 -0500

    fill in artist table

--- db/migrate/20220622213349_create_artists_table.rb
+++ db/migrate/20220622213349_create_artists_table.rb
@@ -1,4 +1,10 @@
 class CreateArtistsTable < ActiveRecord::Migration[6.1]
   def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :members
+      t.timestamps
+    end
   end
 end
